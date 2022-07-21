d8bd9d9ec642dfe982aa6023edf791a0dc1799ba
SteinerPascal <st_pascal@outlook.com>
2022-07-21 12:14:24 +0200

    add custom table as yasr plugin

--- src/manager/yasgui/yasg.tsx
+++ src/manager/yasgui/yasg.tsx
@@ -1,22 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "Sparnatural/dist/sparnatural.js"
 import "Sparnatural/dist/sparnatural.css"
 import Yasgui from "@triply/yasgui";
 import "@triply/yasgui/build/yasgui.min.css";
 import Tab from "@triply/yasgui/build/ts/src/Tab";
+import * as CustomTable from "./plugins/custom-table/CustomTable";
+
+// custom-table: Table plugin which reroutes clicks on IRIs to react routes
+// map-plugin: Plugin which shows the result of geosparql in a map.
+const registerYasrPlugins = ()=>{
+ Yasgui.Yasr.registerPlugin('custom-table',CustomTable.default as any)
+}
 
 const Yasg = ({onTabChange}:{onTabChange:(changetab:Tab)=>void}) => {
 
-  let el:HTMLElement | null
+  const refContainer = useRef<HTMLElement | null>(null);
   useEffect(() => {
-    el = document.getElementById("yasgui")
-    console.dir(el)
-    if(el){
-        console.dir(Yasgui.defaults)
-      let yasg =new Yasgui(el, { requestConfig: { endpoint: "http://example.com/sparql" }, copyEndpointOnNewTab: false});
-
+    refContainer.current = document.getElementById("yasgui")
+    if(refContainer.current && (refContainer.current.children.length === 0)){
+      registerYasrPlugins()
+      Yasgui.Yasr.plugins.table.defaults.openIriInNewWindow = false
+      let yasg =new Yasgui(refContainer.current, { requestConfig: { endpoint: "http://localhost:7200/repositories/geneva-example" }, copyEndpointOnNewTab: true});
       let initTab = yasg.getTab()
       if(initTab){
+        initTab.getYasr().config.pluginOrder = [
+          "table",
+          "response",
+          "boolean",
+          "error"
+      ]
         onTabChange(initTab)
       } else {
         throw Error('initTab not found in Yasgui component')
